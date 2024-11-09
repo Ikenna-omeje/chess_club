5e7e181bfa73856c66d9ec844ffc0ff7af93b5fe
Ikenna Omeje <karl.ikenna@outlook.com>
2024-11-09 18:03:18 +0000

    add validation to check value do not exceed 4

--- run.py
+++ run.py
@@ -15,16 +15,39 @@ SHEET = GSPREAD_CLIENT.open('chess_club')
 players = SHEET.worksheet('players')
 data = players.get_all_values()
 
+
+def validate_data(values):
+    """
+    This try converts all string values to int.
+    pops up ValueError when its impossible, or if
+    more than 4 values appear.
+    """
+    try:
+        if len(values) != 4:
+            raise ValueError(f"4 value needed, you provided is {len(values)}"
+
+            )
+    except ValueError is e:
+        print(f"invalid data: {e}, please try again!\n")
+
+
+
+    print(values)
+
 def get_players_data():
     """
     get players info from the available data
     """
     print("Enter the players data on monday.")
     print("Data should be four numbers separated by commas.")
-    print("Example: 10,20,30,40")
+    print("Example: 10,20,30,40\n")
 
-data_str = input("enter your data here: ")
-print(f"the data entered is {data_str}")
+data_str = input("Enter your data here: ")
+
+players_data = data_str.split(",")
+validate_data(players_data)
+
 
 get_players_data()
+
 
