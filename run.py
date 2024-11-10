0706e84e0064d8671474b597e52fa35d3af96e94
Ikenna Omeje <karl.ikenna@outlook.com>
2024-11-10 15:53:51 +0000

    adjusted the functions for collecteng and converting strings to integers

--- run.py
+++ run.py
@@ -12,40 +12,48 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('chess_club')
 
-players = SHEET.worksheet('players')
-data = players.get_all_values()
 
 
 
+
+ # Get players' info from the available data.    
+def get_players_week_data():
     
-def get_players_data():
-    """
-    Get players' info from the available data, looping until valid data is provided.
+    players = SHEET.worksheet('players')
+
+    data = players.get_all_values().get_all_values()[-7:]
+    #converting strings to integers
+    return [int[(num)for num in row] for num in players]
+
+
+def get_games_data():
+    games = SHEET.worksheet('players')
+
+    data = games.get_all_values().get_all_values()[-7:]
+    #converting strings to integers
+    return [int[(num)for num in row] for num in games]
+
+
     """
     while True:
-        print("Enter the players' data for Monday.")
-        print("Data should be four numbers separated by commas.")
-        print("Example: 10,20,30,40\n")
+        print("maximum players per day:", max_players_per_day)
+        print("max game per day (assumimg 12min per game):", max_game_per_day)
+        print("total maximum players in
 
         # Prompt the user for input
-        data_str = input("Enter your data here: ")
-
-        # Split the input string by commas to get individual values     
-        players_data = data_str.split(",")
-
-        # Validate the data
-        if validate_data(players_data):
-            print("Data is valid!")
-            break  # Exit the loop once data is valid
+        data_str =
 
     return players_data
 
+    """
+
 def validate_data(values):
-    """
+    
     Convert all string values to integers. Raises a ValueError if conversion fails or if more than 4 values are provided.
-    """
+    
     try:
         # Convert values to integers
+
         [int(value) for value in values]
         if len(values) != 4:
             raise ValueError(
@@ -56,30 +64,29 @@ def validate_data(values):
         return False
 
     return True
-
+"""
 # Call the function to get the data
-get_players_data()
+#get_players_data()
 
-def get_games_data():
+
     """
     Get games info from the data
     """
-    games = SHEET.worksheet('games')
+     games = SHEET.worksheet('games')
     data = games.get_all_values()
 
     print(data)  # Display games data
     return data
+   
 
-def max_studio_players(players_data):
+def max_all_studio_players():
     """
     cal maximum amount of studio players per day and 
     maximum for a week
     """
-    players = SHEET.worksheet('players').get_all_values()
-    players_row = players[-7]
-    print(players_row)
-    
-    total_players_per_day = [max(map(int, day)) for day in players_data]
+    players = SHEET.worksheet('players').get_all_values()[-7:]
+
+    total_players_per_day = [max(map(int, day)) for day in players]
 
     max_players_week = sum(total_players_per_day)
 
@@ -87,11 +94,13 @@ def max_studio_players(players_data):
     
     return max_players_week
 
+"""
     #fetch game data and cal maximum game played in a week
 players_data = get_players_data()
 max_studio_players(players_data)
-
 """
+
+
 def max_studio_games(games_data):
     
    # cal maximum amount of sudio games per day and 
@@ -107,7 +116,6 @@ def max_studio_games(games_data):
     #fetch game data and cal maximum game played in a week
 games_data = get_games_data()
 max_studio_games(games_data)
-"""
 
 
-
+def daily_rewards():
