32a359af78c8f0e21707fa721c9c273db42b4260
Ikenna Omeje <karl.ikenna@outlook.com>
2024-11-11 05:10:19 +0000

    fixed a lot of white and blank lines.

--- run.py
+++ run.py
@@ -13,19 +13,23 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('chess_club')
 
 
- # Get players' info from the available data.    
+"""
+fetch players and number of games info from the available chess-club data.
+"""
+
+
 def get_players_data():
-    
     players = SHEET.worksheet('players').get_all_values()[-7:]
-    #converting strings to integers
+    # converting strings to integers
     return [[int(num)for num in row] for row in players]
 
 
 def get_games_data():
 
     games = SHEET.worksheet('games').get_all_values()[-7:]
-    #converting strings to integers
+    # converting strings to integers
     return [[int(num)for num in row] for row in games]
+
 
 def input_new_data():
     """
@@ -35,13 +39,12 @@ def input_new_data():
     print("Enter players'and game data for a single day.")
     print("Data should be four numbers separated by commas.")
     print("Example: 10,20,30,40\n")
-    
 
-    #input single player data
+    # input single player data
     player_data_input = input("enter player data here: ")
     player_data = [int(num) for num in player_data_input.split(',')]
 
-    #input single games data
+    # input single games data
     game_data_input = input("enter game data here: ")
     game_data = [int(num) for num in game_data_input.split(',')]
 
@@ -52,42 +55,36 @@ def calculating_max_totals(players_data, games_data):
     """
     cal maximum amount of studio players per day and for a week
     cal maximum amount of games per day and for a week
-
     """
-    #total for players in a day an week
+    # total for players in a day an week
 
-    max_players_per_day = [max(day) for day in players_data]
+    players_day = [max(day) for day in players_data]
+    games_day = [max(day) for day in games_data]
 
-    max_games_per_day = [max(day) for day in games_data]
+    # totals for games in a day and a week
+    t_players_week = sum(players_day)
+    t_games_week = sum(games_day)
 
+    print(f"Maximum players per in a week: {players_day}")
+    print(f"maximum games per day: {games_day}")
+    print(f"total max game week: {t_games_week}")
+    print(f"total maximum players week: {t_players_week}")
 
-    #totals for games in a day and a week
-
-    total_max_player_week = sum(max_players_per_day)
-
-    total_max_games_week = sum(max_games_per_day)
-
-    print(f"Maximum players per in a week: {max_players_per_day}")
-    print(f"maximum games per day: {max_games_per_day}")
-    print(f"total max game week: {total_max_games_week}")
-    print(f"total maximum players week: {total_max_player_week}") 
+    return players_day, games_day, t_players_week, t_games_week
 
 
-    return max_players_per_day, max_games_per_day, total_max_player_week, total_max_games_week
-
-    #calculate average game time and rewards
-def calculate_daily_statistics(max_games_per_day, max_players_per_day):
+# calculate average game time and rewards
+def calculate_daily_statistics(games_day, players_day):
     hours_per_day = 5
     total_minutes = hours_per_day * 60
 
-
-    for i, games in enumerate(max_games_per_day):
+    for i, games in enumerate(games_day):
         if games > 0:
-            avg_game_time = total_minutes / games #average time per game
+            avg_game_time = total_minutes/games  # average time per game
         else:
-            avg_game_time = 0 
+            avg_game_time = 0
 
-        muffins_per_person = max_players_per_day[i] # 1 muffin per person
+        muffins_per_person = players_day[i]  # 1 muffin per person
         daily_rewards = games * 10   # 10 per game won
 
         print(f"Day {i+1}:")
@@ -95,18 +92,20 @@ def calculate_daily_statistics(max_games_per_day, max_players_per_day):
         print(f"Muffins given: {muffins_per_person}")
         print(f"Total dollars awarded: ${daily_rewards}")
 
+
 # Execute the calculations
-new_data_choice = input("Would you like to input new data? (yes/no): ").strip().lower()
+new_data_choice = input(
+    "Would you like to input new data? (yes/no): ").strip().lower()
 
 if new_data_choice == "yes":
     players_data, games_data = input_new_data()
 else:
-    games_data = get_games_data()   
+    games_data = get_games_data()
     players_data = get_players_data()
 
-max_players_per_day, max_games_per_day, total_max_players_week, total_max_games_week = calculating_max_totals(players_data, games_data)
-
-calculate_daily_statistics(max_games_per_day, max_players_per_day)
+players_day, games_day, t_players_week, t_games_week = calculating_max_totals(
+    players_data, games_data)
+calculate_daily_statistics(games_day, players_day)
     
 
 
